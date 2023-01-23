6f61b2201b993f760d68deb33f1144e147ba8783
Marcelo Moll <75146662+MollMarceloMoll@users.noreply.github.com>
2023-01-23 19:52:29 -0300

    Ejercicio
    Enunciado del ejercicio:
    
    Crea un archivo JS que contenga las siguientes líneas
    
    - Una cadena de texto con tu Nombre
    
    - Otra cadena de texto con tu Apellido
    
    - Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
    
    - Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas
    
    - Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
    
    - Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
    
    - Una variable que contenga la primera letra del Nombre
    
    - Otra variable que contenga la última letra del Apellido
    
    - Una cadena de texto que elimine los espacios de la variable "estudiante"
    
    - Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"

---
+++ ejercicio-string.js
@@ -0,0 +1,29 @@
+let nombre = "Marcelo";
+console.log("nombre")
+
+let apellido = "Moll";
+console.log(apellido)
+
+let estudiante = nombre +" "+ apellido;
+console.log(estudiante)
+
+let estudianteMayus = (estudiante.toUpperCase());
+console.log(estudianteMayus)
+
+let estudianteMinus = (estudiante.toLowerCase());
+console.log(estudianteMinus)
+
+let estudianteNume = (estudiante.length);
+console.log(estudianteNume);
+
+let estudiantePrime = (estudiante[0]);
+console.log(estudiantePrime)
+
+let estudianteUlti = (estudiante[11]);
+console.log(estudianteUlti)
+
+let estudianteEspa = (estudiante.trim());
+console.log(estudianteEspa)
+
+let estudianteBoole = (estudiante.includes(nombre));
+console.log(estudianteBoole)
